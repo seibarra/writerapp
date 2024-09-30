1a089612265de5d71bea0ae639004081bb71d046
Sebastián Ibarra <sebastianibarra104@gmail.com>
2024-09-29 23:06:22 -0300

    Fix package versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
 
-  modules: ['@pinia/nuxt', '@nuxt/ui', 'nuxt-zod-i18n', '@nuxtjs/i18n', "nuxt-vuefire"],
+  modules: ['@pinia/nuxt', '@nuxt/ui', 'nuxt-zod-i18n', '@nuxtjs/i18n'],
   css: [ '~/assets/css/main.css' ]
 })
